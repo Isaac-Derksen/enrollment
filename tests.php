8e5c10ca635c015a9e402d77d95d7d330cb5e36c
PROGRAMMING\ebruce <cvtechonline@cvtechonline.net>
2021-04-26 13:17:39 -0500

    address start

---
+++ tests.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form action="tests.php" method="POST"></form>
+    <label for="street">Address: </label>
+    <input type="text" name="street" id="address">
+    <label for="city">Ciy: </label>
+    <input type="text" name="street" id="street">
+</body>
+</html>
