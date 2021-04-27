ddb6e74523cb45ca0b13ec7357cf7aac945ce84c
PROGRAMMING\ebruce <cvtechonline@cvtechonline.net>
2021-04-27 13:49:12 -0500

    changed names and made the student contact

---
+++ studentcontact.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+    <form action="tests.php" method="POST">
+    <label for="studentid">Student ID:</label>
+    <input type="number" name="StudentID" id="studentID">
+    <label for="Name">Name:</label>
+    <input type="text" name="Name" id="Name">
+    <label for="Relationship">Relationship: </label>
+    <input type="text" name="Relationship" id="Relationship">
+    <label for="Phonenumber">Phone Number: </label>
+    <input type="text" name="Phonenumber" id="Phonenumber">
+    <input type="submit" value="submit">
+    </form>
+</body>
+</html>
+<?PHP
+    function add(){
+        $server = "localhost";
+        $user = "root";
+        $pass = "";
+        $db = "test";
+
+        $conn = new mysqli($server, $user, $pass, $db);
+        $sql = strtr("INSERT INTO studentcontact(studentID,name,relationship,Phonenumber) values ('%StudentID','%Name','%Relationship','%Phonenumber');",
+        ['%StudentID' => $_POST['StudentID'], '%Name' => $_POST['Name'], '%Relationship' => $_POST['Relationship'], '%Phonenumber' => $_POST['Phonenumber']]);
+        $conn->query($sql);
+    }
+    if (isset($_POST['Name'])) add();
+
+?>
