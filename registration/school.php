242376544ea80e76c4fdb4526b3dcd4bc27d306f
Isaac-Derksen <77300634+Isaac-Derksen@users.noreply.github.com>
2021-05-03 12:28:49 -0500

    .

--- registration/school.php
+++ registration/school.php
@@ -66,6 +66,13 @@
         if (/* empty($name_error) && empty($district_error) && empty($street_error) && empty($city_error) && empty($state_error) && empty($zip_error) */ true) {
             // $sql = "SELECT AddressID FROM Address WHERE Street = ? AND City = ? AND State = ? AND Zipcode = ?";
 
+            echo $name;
+            echo $district;
+            echo $street;
+            echo $city;
+            echo $state;
+            echo $zip;
+            
             // if ($stmt = $conn->prepare($sql)) {
             //     $stmt->bind_param("ssss", $param_street, $param_city, $param_state, $param_zip);
 
@@ -103,21 +110,21 @@
             //     }
             // }
             
-            $sql = "INSERT INTO School VALUES (?, ?, ?);";
+            // $sql = "INSERT INTO School VALUES (?, ?, ?);";
 
-            if ($stmt = $conn->prepare($sql)) {
-                $stmt->bind_param("ssi", $param_name, $param_district, $param_addr);
+            // if ($stmt = $conn->prepare($sql)) {
+            //     $stmt->bind_param("ssi", $param_name, $param_district, $param_addr);
 
-                $param_name = $name;
-                $param_district = $district;
-                $param_addr = 0;
+            //     $param_name = $name;
+            //     $param_district = $district;
+            //     $param_addr = 0;
 
-                if ($stmt->execute()) {
-                    echo "record Created";
-                } else {
-                    echo "Something went wrong, please try again.";
-                }
-            }
+            //     if ($stmt->execute()) {
+            //         echo "record Created";
+            //     } else {
+            //         echo "Something went wrong, please try again.";
+            //     }
+            // }
         }
         $conn->close();
     }
